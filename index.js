f26b740b7b860fb6a4c48afd14f916db20e0ca3c
Christopher Anderson <andersonchris227@gmail.com>
2018-08-27 19:45:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,16 +1,4 @@
-var uppercase = "HELLO!"
- 
-uppercase.toUpperCase() === uppercase // true
- 
-var lowercase = 'hello!'
- 
-lowercase.toLowerCase() === lowercase // true
- 
-var mixedCase = 'Hi there!'
- 
-mixedCase.toLowerCase() === mixedCase // false
- 
-mixedCase.toUpperCase() === mixedCase // false
+
 
 function shout(string) {
   return string.toUpperCase ()
@@ -23,6 +11,14 @@ function logShout(string) {
 }
 function logWhisper(string) {
   console.log (string.toLowerCase ())
+} 
+
+function sayHiToGrandma(string) {
+  if (string.toLowerCase() === string) {
+    return "I can\'t hear you!";
+  } else if (string.toUpperCase() === string) {
+    return "YES INDEED!";
+  } else if (string === "I love you, Grandma.") {
+    return "I love you, too.";
+  }
 }
-
-
